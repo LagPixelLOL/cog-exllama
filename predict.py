c373ad41ccba20a56e8d5267280c1b9836d8fafa
v2ray <2282688304@qq.com>
2023-07-29 13:40:40 +0000

    Initialize.

---
+++ predict.py
@@ -0,0 +1,11 @@
+from cog import BasePredictor, Input, Path
+
+class Predictor(BasePredictor):
+    
+    def setup(self) -> None:
+        return
+
+    def predict(
+        self,
+    ) -> None:
+        print("Hello, world!")
